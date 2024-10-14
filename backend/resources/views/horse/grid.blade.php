de51942c0766b217e87addb4eed73881a778be0f
B4lint0413 <balu050413@gmail.com>
2024-10-14 10:48:04 +0000

    view fundamentals

---
+++ backend/resources/views/horse/grid.blade.php
@@ -0,0 +1,1 @@
+grid.blade.php
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HorseController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get("/", [HorseController::class, "index"])->name("home");
+Route::get("/horse/list", [HorseController::class, "list"])->name("horse.list");
+Route::get("/horse/table", [HorseController::class, "table"])->name("horse.table");
+Route::get("/horse/grid", [HorseController::class, "grid"])->name("horse.grid");
